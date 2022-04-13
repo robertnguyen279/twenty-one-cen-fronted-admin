9aa0daf01b6497d08501d83810559582e120a5ee
Robert Nguyen <robert.nguyen279@gmail.com>
2022-04-13 16:43:12 +0700

    Add nontification icon

---
+++ src/components/RightHeader/RightHeader.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NontificationIcon from 'assets/icons/bell-svgrepo-com.svg';
+
+const RightHeader = (): React.ReactElement => {
+  return (
+    <div className="right_header flex pt-10 justify-end pr-10">
+      <div className="icon">
+        <img src={NontificationIcon} alt="nontification" />
+      </div>
+    </div>
+  );
+};
+
+export default RightHeader;
--- src/pages/DashBoard/Header.tsx
+++ src/pages/DashBoard/Header.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import RightHeader from 'components/RightHeader';
 
 const Header = (): React.ReactElement => {
-  return <div className="header">Header</div>;
+  return (
+    <div className="header">
+      <div className="right_header">
+        <RightHeader />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
